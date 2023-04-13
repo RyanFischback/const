f12524b4cd837f9f16c966a2cfbd66b79b90f0d3
rfischback <ryan-fischback@hotmail.com>
2023-04-13 15:34:40 -0300

    project setup, folder structure, home and nav page routes

---
+++ frontend/src/views/Home/Home.js
@@ -0,0 +1,12 @@
+import Navigation from "../Navigation/Navigation"
+
+//Home page will have a Navigation bar, side scrolling images for projects that create a sidemenu popup for additional information about that project. (Then afterwards have each project link to its own project page with additional images and information, kinda blog esq)
+const Home = () => {
+    return (
+        <div>
+        <Navigation />
+        </div>
+    );
+};
+
+export default Home;
