0016a9cacec62586e4156f7bd78890d90d144cfd
rfischback <ryan-fischback@hotmail.com>
2023-04-22 18:12:02 -0300

    login and project route added to routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './views/Login/Login';
+import Project from './views/Project/Project';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes ,Route }from "react-router-dom";
 
@@ -10,6 +12,8 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/project" element={<Project/>} />
     </Routes>
   </BrowserRouter>
 );
