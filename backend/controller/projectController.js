004c1e8cdf259f303726e93e4abee8419612d47b
rfischback <ryan-fischback@hotmail.com>
2023-04-22 18:06:46 -0300

    Setting up project controller structure

--- backend/controller/projectController.js
+++ backend/controller/projectController.js
@@ -1,1 +1,22 @@
-const {db} = require('../database/db');
+const Project = require('../models/projectModel')
+
+const getProjects = async (req, res, next) => {
+
+}
+
+const updateProject = async (req, res, next) => {
+    
+}
+const createProject = async (req, res, next) => {
+    
+}
+const deleteProject = async (req, res, next) => {
+    
+}
+
+module.exports = {
+    getProjects,
+    updateProject,
+    createProject,
+    deleteProject
+}
--- frontend/src/views/Navigation/Navigation.js
+++ frontend/src/views/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 const navigation = () => {
     return (
         <div>
-
+            
         </div>
     );
 };
