7a8cdd64e53816553e6f22ad44aad564b5b7c956
rfischback <ryan-fischback@hotmail.com>
2023-04-23 19:18:02 -0300

    Wow! Links! Groundbreaking!

---
+++ frontend/src/views/Project/Project.jsx
@@ -0,0 +1,11 @@
+import Navigation from "../Navigation/Navigation"
+const Project = () => {
+    return (
+    <>
+    <Navigation />
+    <p>Project Page</p>
+    </>
+    );
+};
+
+export default Project;
