44f09979127109bfbf49455d204eb3e7dc51be50
rfischback <ryan-fischback@hotmail.com>
2023-04-24 16:24:13 -0300

    nav links all setup, login page styling, random home logo for now, player profile route created

--- frontend/src/views/Navigation/Navigation.jsx
+++ frontend/src/views/Navigation/Navigation.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
+import logo from './logo.png';
 
 const Navigation = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+  // function to handle logging out
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+    }
+    // function to handle logging in
+    const handleLogin = () => {
+        setIsLoggedIn(true);
+    }
     return (
     <>
         <header id="navbar-container">
             <div id="navbar-left">
                 <Link to="/" id="nav-logo">
-                    <img src="./logo.png" alt="Home" />
+                    <img src={logo} alt="Home" />
                 </Link>
             </div>
             <ul id="navbar-right">
-                <li>
-                    <Link to="/login" className="nav-link">Login</Link>
-                </li>
+                {!isLoggedIn && <li><button className="button-link nav-link" onClick={handleLogin}>Login</button></li>}
                 <li>
                     <Link to="/project" className="nav-link">Projects</Link>
                 </li>
-                {/* <li>
-                    <Link to="/login" className="nav-link">Profile</Link>
-                </li> */}
+                {isLoggedIn && <li><Link to="/profile" className="nav-link">Profile</Link></li>}
+                {isLoggedIn && <li><button className="button-link nav-link" onClick={handleLogout}>Logout</button></li>}
             </ul>
         </header>
     </>
