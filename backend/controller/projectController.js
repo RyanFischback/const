d5b97cd1c562379b7e7d6d84164a5b115ab567a8
rfischback <ryan-fischback@hotmail.com>
2023-05-04 17:52:25 -0300

    fixed register/jwt bug, pretty bad of me to show .env but oh well

--- backend/controller/projectController.js
+++ backend/controller/projectController.js
@@ -1,9 +1,10 @@
 const Project = require('../models/projectModel')
+const asyncHandler = require('express-async-handler')
 
-const getProjects = async (req, res, next) => {
-
-}
-
+const getProjects = asyncHandler(async (req, res) => {
+    const projects = await Project.find()
+    res.json(projects)
+})
 
 module.exports = {
     getProjects
