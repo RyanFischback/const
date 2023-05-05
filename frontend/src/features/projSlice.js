3ee51e3d5409e21c42b7ae6820c3f9c8dac94b39
rfischback <ryan-fischback@hotmail.com>
2023-05-05 16:03:51 -0300

    added some css for the projects and fixed the projects bug (i wasnt returning it in the slice)....lol

--- frontend/src/features/projSlice.js
+++ frontend/src/features/projSlice.js
@@ -11,7 +11,7 @@ const initialState = {
 
 export const display = createAsyncThunk('proj/display', async () => {
     console.log("awaiting display projects slice")
-    await projService.display()
+    return await projService.display()
 })
 
 export const projSlice = createSlice({
--- frontend/src/views/Home/Home.jsx
+++ frontend/src/views/Home/Home.jsx
@@ -1,15 +1,16 @@
-import {useEffect} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-import { display } from '../../features/projSlice';
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { display } from '../../features/projSlice'
+import './Home.css'
 
 function Dashboard() {
-  const { user } = useSelector((state) => state.auth);
-  const { isLoading, projects } = useSelector((state) => state.proj);
-  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth)
+  const { isLoading, projects } = useSelector((state) => state.proj)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(display());
-  }, [dispatch]);
+    dispatch(display())
+  }, [dispatch])
 
   return (
     <>
@@ -17,23 +18,25 @@ function Dashboard() {
         <h1>Welcome {user && user.name}</h1>
       </section>
 
-
-    <section className='content'>
-      <h3>Projects</h3>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : projects ? (
-        <ul>
-          {projects.map((project) => (
-            <li key={project.id}>{project.name}</li>
+      <section className='content'>
+        <h3>Projects</h3>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : projects && projects.length > 0 ? (
+          <div className='project-grid'>
+          {projects.map((project, index) => (
+            <div key={`project-${index}`} className='project-tile'>
+              <h4>{project.name}</h4>
+              <p>{project.description}</p>
+            </div>
           ))}
-        </ul>
-      ) : (
-        <p>No projects found.</p>
-      )}
-    </section>
+          </div>
+        ) : (
+          <p>No projects found.</p>
+        )}
+      </section>
     </>
-  );
+  )
 }
 
-export default Dashboard;
+export default Dashboard
