41459e8d560242c4dab684572b797af1759f4cdb
rfischback <ryan-fischback@hotmail.com>
2024-03-18 22:56:15 -0300

    remove find() and add todo

--- backend/controller/projectController.js
+++ backend/controller/projectController.js
@@ -3,7 +3,9 @@ const asyncHandler = require('express-async-handler')
 
 const getProjects = asyncHandler(async (req, res) => {
     console.log("awaiting get projects controller")
-    const projects = await Project.find()
+    // todo: look into type Project? .find needs a pointer to refer to
+    // for now removing find
+    const projects = await Project
     console.log(projects[0])
     res.json(projects)
 })
